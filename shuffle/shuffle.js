0f4e0043a178630a44cf3ae1dd657ed52f195278
Seera Kaenkaew <jay@sennalabs.com>
2021-01-04 17:37:29 +0700

    test(shuffle): create shuffle

--- shuffle/shuffle.js
+++ shuffle/shuffle.js
@@ -4,6 +4,18 @@
  * @return {number[]}
  */
 export function shuffle(cards) {
-  throw new Error('Not implemented')
-}
+  let currentIndex = cards.length;
+  let randomIndex;
+  let temp;
 
+  while (currentIndex > 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    temp = cards[currentIndex];
+    cards[currentIndex] = cards[randomIndex];
+    cards[randomIndex] = temp;
+  }
+
+  return cards;
+}

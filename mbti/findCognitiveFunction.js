d22bb00d39d35f55ecfedd8008ceeb017cead5e2
Seera Kaenkaew <jay@sennalabs.com>
2021-01-05 10:12:16 +0700

    test(mbti): หาค่า dominant และ auxiliary จาก second และ third

--- mbti/findCognitiveFunction.js
+++ mbti/findCognitiveFunction.js
@@ -9,9 +9,19 @@
  */
 export function findCognitiveFunction(mbti) {
   let [first, second, third, fourth] = mbti;
+  let dominant = "";
+  let auxiliary = "";
 
   second += fourth.toLowerCase() === "p" ? "e" : "i";
   third += fourth.toLowerCase() === "j" ? "e" : "i";
+
+  if (first.toLowerCase() === second[1]) {
+    dominant = second;
+    auxiliary = third;
+  } else {
+    dominant = third;
+    auxiliary = second;
+  }
 
   return {};
 }
